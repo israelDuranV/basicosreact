6d48d15c917b74510ffdef428b120893c1962d6a
israelDuranV <israel_duran@outlook.com>
2019-01-10 22:04:32 -0600

    Experimentando con React y objetos json

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const trabajador = {
+      nombre: "Israel",
+      trabajo: "Desarrollador",
+      domicilio: "Niños Heroes N 123"
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className="contenedor">
+          <h1>Aprendiendo React</h1>
+          <p>Nombre {trabajador.nombre}</p>
+          <p>Trabajo {trabajador.trabajo}</p>
+          <p>Trabajo {trabajador.domicilio}</p>
+          </div>
     );
-  }
+  } 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const titulo = React.createElement('h1',{id: 'titulo,classname: ''})
-
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
